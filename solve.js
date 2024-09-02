bb3c7fad296dbb1a11691cfc589e87cd1747f6dd
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-03 04:03:33 +0600

    problem-2 added

--- solve.js
+++ solve.js
@@ -1,6 +1,8 @@
-//Print Randomly (1 to 6) for LUDU
+//Print Randomly (1 to 6) for LUDU -------PROBLEM-------1
 function getRandBetweenMinAndMax(min,max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
 console.log(getRandBetweenMinAndMax(1,6))
+
+//Print Randomly (1 to 6) for LUDU -------PROBLEM-------2
