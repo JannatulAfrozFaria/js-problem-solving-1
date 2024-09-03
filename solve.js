b13d0cbf691840708032feea3efcc68a6ec1f282
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-04 02:58:40 +0600

    count introduced

--- solve.js
+++ solve.js
@@ -16,29 +16,33 @@
 
 //How to arrange the students' roll numbers in an ascending order?------PROBLEM---3
 //---------------------------------------------------------------------
-const roll_numbers = [4,6,1,7,13,11,18,26,56,34,31,45,47,87,86,84]
-console.log(roll_numbers.sort(function(a,b){
-    return a - b;
-})); //sort changes the main array
+// const roll_numbers = [4,6,1,7,13,11,18,26,56,34,31,45,47,87,86,84]
+// console.log(roll_numbers.sort(function(a,b){
+//     return a - b;
+// })); 
+//sort changes the main array
 
 
 
 //How to identify Leap Year?------PROBLEM---4
 //---------------------------------------------------------------------
-function isLeapYear(year){
-    if(year % 400 === 0 || (year % 4 === 0 && year % 100!== 0 )){
-        console.log(`${year} is a leap year`)
-    } else {
-        console.log(`${year} is not a leap year`)
-    }
-}
-
-isLeapYear(2026);
+// function isLeapYear(year){
+//     if(year % 400 === 0 || (year % 4 === 0 && year % 100!== 0 )){
+//         console.log(`${year} is a leap year`)
+//     } else {
+//         console.log(`${year} is not a leap year`)
+//     }
+// }
+// isLeapYear(2026);
 
 
 //How to check the total number of vowels in a sentence? ------------PROBLEM---5
 //---------------------------------------------------------------------
 const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+function countVowels(sentence){
+    let count = 0;
+    console.log(`There are ${count} vowels in this sentence`)
+}
 //---------------------------------------------------------------------
 
 
