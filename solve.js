9e6a34cb22ca5d8d90bf957f582812d7f43e8202
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-04 03:28:23 +0600

    problem 5 solved

--- solve.js
+++ solve.js
@@ -42,7 +42,13 @@ const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 function countVowels(sentence){
     let count = 0;
     const letters = Array.from(sentence);
+    letters.forEach(function(value,index,array){
+        if(vowels.includes(value)){
+            count++;
+        }
+    })
     console.log(`There are ${count} vowels in this sentence`)
+    return count;
 }
 countVowels("I love my country")
 //---------------------------------------------------------------------
