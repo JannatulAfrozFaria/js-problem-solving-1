4e6b719e9aa0078784164c092815259395f7ecb4
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-04 02:51:20 +0600

    problem-4 solved

--- solve.js
+++ solve.js
@@ -23,11 +23,17 @@ console.log(roll_numbers.sort(function(a,b){
 
 
 
-//---------------------------------------------------------------------
 //How to identify Leap Year?------PROBLEM---4
 //---------------------------------------------------------------------
+function isLeapYear(year){
+    if(year % 400 === 0 || (year % 4 === 0 && year % 100!== 0 )){
+        console.log(`${year} is a leap year`)
+    } else {
+        console.log(`${year} is not a leap year`)
+    }
+}
 
-
+isLeapYear(2026);
 
 
 //---------------------------------------------------------------------
